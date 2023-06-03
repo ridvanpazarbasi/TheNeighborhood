65b3542083c712b753b3e1f7ccebe5f97e0d284e
ridvanpazarbasi <68926961+ridvanpazarbasi@users.noreply.github.com>
2023-06-03 20:13:07 +0300

    fix

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapons : MonoBehaviour
@@ -8,24 +9,49 @@ public class Weapons : MonoBehaviour
     public float range;
     public Camera myCamera;
     public ParticleSystem shootEffect;
+    public ParticleSystem bulletTrack;
+    public ParticleSystem bloodEffect;
+    public List<GameObject> particleSyt;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0) && isGunShoot && Time.time > gunShootFrequency)
-        {
-            GunShoot();
-            gunShootFrequency = Time.time + gunShootFrequencySet;
-        }
+        if (!Input.GetKey(KeyCode.Mouse0) || !isGunShoot || !(Time.time > gunShootFrequency)) return;
+        GunShoot();
+        gunShootFrequency = Time.time + gunShootFrequencySet;
     }
 
     private void GunShoot()
     {
-        RaycastHit hit;
         SoundsManager.Instance.SoundPlay(SoundsType.HandGun);
         shootEffect.Play();
-        if (Physics.Raycast(myCamera.transform.position, myCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(myCamera.transform.position, myCamera.transform.forward, out var hit, range))
         {
             Debug.Log(hit.transform.name);
         }
+
+        ParticleSystem obj;
+        if (hit.transform.gameObject.CompareTag("Enemy"))
+        {
+            obj = Instantiate(bloodEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+        else if (hit.transform.gameObject.CompareTag($"Overturn"))
+        {
+            Rigidbody rg = hit.transform.gameObject.GetComponent<Rigidbody>();
+            rg.AddForce(-hit.normal * 500f);
+            obj = Instantiate(bulletTrack, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+        else
+        {
+            obj = Instantiate(bulletTrack, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+
+        particleSyt.Add(obj.gameObject);
+        if (particleSyt.Count != 20) return;
+        foreach (var t in particleSyt)
+        {
+            Destroy(t);
+        }
+
+        particleSyt.Clear();
     }
 }
